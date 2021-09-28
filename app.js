f4f58aa3199d8a16612eb8395cfeacc8e8f1c1ed
Yannick Severo <severoyannick@gmail.com>
2021-09-28 21:18:22 +0200

    Ecrire en réponse HTTP un premier nombre entre 1 et 100

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // Je jeu de la fourchette avec un serveur http
 const http = require('http');
 const PORT = 3000;
+// Fonction qui génère ue nombre aléatoire
+const getRandom = require('./modules/randomNumber');
 
 // Creation du serveur
 const server = http.createServer((req, res) => {
+  let number = getRandom(1, 101);
   // Reponse du serveur
-  res.end('La réponse du serveur !' );
+  res.end(`Un nombre aléatoire entre 0 et 100 ==> ${number}`);
 });
 // Serveur qui écoute le port 3000
 server.listen(PORT);
