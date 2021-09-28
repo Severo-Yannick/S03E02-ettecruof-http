1c0250365957d4f55d75e1760f81a695b348ebc6
Yannick Severo <severoyannick@gmail.com>
2021-09-28 20:55:04 +0200

    Créer un serveur qui écoute le port 3000

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// TODO
+// Je jeu de la fourchette avec un serveur http
+const http = require('http');
+const PORT = 3000;
+
+// Creation du serveur
+const server = http.createServer((req, res) => {
+  // Reponse du serveur
+  res.end('La réponse du serveur !' );
+});
+// Serveur qui écoute le port 3000
+server.listen(PORT);
