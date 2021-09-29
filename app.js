b74090666843acfd9b125f1af648b94ead186c1b
Yannick Severo <severoyannick@gmail.com>
2021-09-29 09:56:39 +0200

    L'utilisateur devra atteindre une des trois routes suivantes : /plus, /moins, ou /bravo

--- app.js
+++ app.js
@@ -5,10 +5,36 @@ const PORT = 3000;
 const getRandom = require('./modules/randomNumber');
 
 // Creation du serveur
-const server = http.createServer((req, res) => {
+const Response = ((req, res) => {
   let number = getRandom(1, 101);
-  // Reponse du serveur
-  res.end(`Un nombre aléatoire entre 0 et 100 ==> ${number}`);
+  let message = '';
+
+  switch (req.url) {
+    case '/':
+      message = 'Est ce que c\'est plus de 50 ?';
+      break;
+    case '/plus':
+      message = 'Vous dites Plus OK!';
+      break;
+    case '/moins':
+      message = 'Vous dites Moins OK!';
+      break;
+    case '/bravo':
+      message = 'C\'est gagné !';
+      break;
+    case '/number':
+      message = `Nombre aléatoire entre 1 & 100 inclus ==> ${number}`;
+      break;
+
+    default:
+      res.statusCode = 404;
+      message = 'Page non trouvée';
+  }
+  res.write(`<p style="text-align: center;">${message}</p>`);
+  res.end();
 });
+
+// Creation du serveur
+const server = http.createServer(Response);
 // Serveur qui écoute le port 3000
 server.listen(PORT);
